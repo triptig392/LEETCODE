5320027d9e9ed0ce8bdbe14b0232a67765682728
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-10-31 18:56:22 +0530

    Time: 35 ms (77.27%), Space: 44.4 MB (31.13%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
@@ -6,8 +6,8 @@ class Solution {
         Arrays.fill(LDS, 1);
 
         // Compute LIS for each index
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     LIS[i] = Math.max(LIS[i], LIS[j] + 1);
                 }
