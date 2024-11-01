d3acb85441f75f78fa0d0f77e50cdf05fb540de3
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-01 16:35:51 +0530

    Time: 52 ms (29.07%), Space: 45.3 MB (90.84%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,24 +1,28 @@
 class Solution {
+
     public String makeFancyString(String s) {
+        // If the size of the string is less than 3, return it.
+        if (s.length() < 3) {
+            return s;
+        }
+
         StringBuilder sb = new StringBuilder();
-        sb.append(s.charAt(0));
+        // Start by appending the first two characters to StringBuilder.
+        sb.append(s.charAt(0)).append(s.charAt(1));
 
-        char c = s.charAt(0);
-        int rep = 1;
-
-        for(int i=1; i<s.length(); i++){
-            if(c == s.charAt(i)){
-                rep++;
-            }else{
-                rep = 1;
-                c = s.charAt(i);
-            }
-
-            if(rep <= 2){
+        // Iterate from the 3rd character onwards.
+        for (int i = 2; i < s.length(); ++i) {
+            // If the current character is not equal to the previously inserted
+            // two characters, then we can add it to the StringBuilder.
+            if (
+                s.charAt(i) != sb.charAt(sb.length() - 1) ||
+                s.charAt(i) != sb.charAt(sb.length() - 2)
+            ) {
                 sb.append(s.charAt(i));
             }
         }
 
+        // Convert StringBuilder back to String and return.
         return sb.toString();
     }
 }
