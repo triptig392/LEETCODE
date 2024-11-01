b2bd6434175ab6206537135a55a3c8b6b8a71a2a
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-01 16:30:42 +0530

    Time: 30 ms (83.72%), Space: 45.5 MB (63.92%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,25 +1,24 @@
 class Solution {
-
     public String makeFancyString(String s) {
-        char prev = s.charAt(0);
-        int frequency = 1;
-        StringBuilder ans = new StringBuilder();
-        ans.append(s.charAt(0));
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == prev) {
-                // If the current character is equal to the previous character, increment the
-                // frequency.
-                frequency++;
-            } else {
-                // Otherwise, we can restart the frequency counter with 1, and store the current
-                // character's value in prev.
-                prev = s.charAt(i);
-                frequency = 1;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+
+        char c = s.charAt(0);
+        int rep = 1;
+
+        for(int i=1; i<s.length(); i++){
+            if(c == s.charAt(i)){
+                rep++;
+            }else{
+                rep = 1;
+                c = s.charAt(i);
             }
-            // If the frequency counter has value less than 3, add the character to the
-            // answer string.
-            if (frequency < 3) ans.append(s.charAt(i));
+
+            if(rep <= 2){
+                sb.append(s.charAt(i));
+            }
         }
-        return ans.toString();
+
+        return sb.toString();
     }
 }
