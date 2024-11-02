60f40baafb93d101ea229c2e74f7ea39a2cfaefc
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-02 23:17:30 +0530

    Time: 1 ms (96.81%), Space: 41.9 MB (54.87%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,17 +1,11 @@
-class Solution {
+public class Solution {
+
     public boolean isCircularSentence(String sentence) {
-        String[] s = sentence.split(" ");
-
-        int n = s.length;
-        if(s[0].charAt(0) != s[n-1].charAt(s[n-1].length()-1)){
-            return false;
-        }
-
-        for(int i=0; i<n-1; i++){
-            if(s[i].charAt(s[i].length()-1) != s[i+1].charAt(0)){
+        for (int i = 0; i < sentence.length(); ++i) {
+            if (sentence.charAt(i) == ' ' && sentence.charAt(i - 1) != sentence.charAt(i + 1)){
                 return false;
             }
         }
-        return true;
+        return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
     }
 }
