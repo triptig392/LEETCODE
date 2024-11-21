e819be50795e752ad9a99217031efb6be1ee2f8f
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-21 20:06:25 +0530

    Time: 32 ms (74.16%), Space: 62.3 MB (5.12%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -4,24 +4,24 @@ class Solution {
         long ans = 0;
         long currentSum = 0;
         int l = 0;
-        int end = 0;
+        int r = 0;
 
         HashMap<Integer, Integer> numToIndex = new HashMap<>();
 
-        while (end < nums.length) {
-            int currNum = nums[end];
+        while (r < nums.length) {
+            int currNum = nums[r];
             int lastOccurrence = numToIndex.getOrDefault(currNum, -1);
             // if current window already has number or if window is too big, adjust window
-            while (l <= lastOccurrence || end - l + 1 > k) {
+            while (l <= lastOccurrence || r - l + 1 > k) {
                 currentSum -= nums[l];
                 l++;
             }
-            numToIndex.put(currNum, end);
-            currentSum += nums[end];
-            if (end - l + 1 == k) {
+            numToIndex.put(currNum, r);
+            currentSum += nums[r];
+            if (r - l + 1 == k) {
                 ans = Math.max(ans, currentSum);
             }
-            end++;
+            r++;
         }
         return ans;
     }
