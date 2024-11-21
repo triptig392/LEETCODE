ceeeded553aa4cc1544073fe3800a075f5e977db
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-21 19:55:27 +0530

    Time: 30 ms (79.95%), Space: 62.4 MB (5.12%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,18 +1,28 @@
 class Solution {
-    public long maximumSubarraySum(int[] A, int k) {
-    HashMap<Integer, Integer> mp = new HashMap<>();
-    long mx = 0, sum = 0;
-    for (int i = 0; i < A.length; i++){
-        sum +=A[i];
-        mp.put(A[i], mp.getOrDefault(A[i],0) + 1);
 
-        if (i >= k - 1){
-            if (mp.size() == k) mx = Math.max(mx, sum);
-            sum -= A[i - k + 1];
-            mp.put(A[i - k + 1], mp.get(A[i - k + 1]) - 1);
-            if (mp.get(A[i - k + 1]) == 0) mp.remove(A[i - k + 1]);
+    public long maximumSubarraySum(int[] nums, int k) {
+        long ans = 0;
+        long currentSum = 0;
+        int begin = 0;
+        int end = 0;
+
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+
+        while (end < nums.length) {
+            int currNum = nums[end];
+            int lastOccurrence = numToIndex.getOrDefault(currNum, -1);
+            // if current window already has number or if window is too big, adjust window
+            while (begin <= lastOccurrence || end - begin + 1 > k) {
+                currentSum -= nums[begin];
+                begin++;
+            }
+            numToIndex.put(currNum, end);
+            currentSum += nums[end];
+            if (end - begin + 1 == k) {
+                ans = Math.max(ans, currentSum);
+            }
+            end++;
         }
+        return ans;
     }
-    return mx;
-}
 }
