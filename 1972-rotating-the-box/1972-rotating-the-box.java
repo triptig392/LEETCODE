8277b63c942ac0790b284a3641caf42c201dea71
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-23 20:18:13 +0530

    Time: 6 ms (97.56%), Space: 79.5 MB (59.61%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.java
+++ 1972-rotating-the-box/1972-rotating-the-box.java
@@ -1,27 +1,36 @@
 class Solution {
+
     public char[][] rotateTheBox(char[][] box) {
-        int r = box.length, c = box[0].length;
-        char[][] box2 = new char[c][r];
-        
-        for(int i = 0; i<r; ++i){
-            int empty = c-1;
-            for(int j = c-1; j>=0; --j){
-                if(box[i][j] == '*'){
-                    empty = j-1;
+        int m = box.length;
+        int n = box[0].length;
+        char[][] result = new char[n][m];
+
+        // Initialize the result grid with '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = '.';
+            }
+        }
+
+        // Apply gravity to let stones fall to the lowest possible empty cell in each column
+        for (int i = 0; i < m; i++) {
+            int lowestRowWithEmptyCell = n - 1;
+            // Process each cell in row `i` in reversed order
+            for (int j = n - 1; j >= 0; j--) {
+                // Found a stone - let it fall to the lowest empty cell
+                if (box[i][j] == '#') {
+                    // Place it in the correct position in the rotated grid
+                    result[lowestRowWithEmptyCell][m - i - 1] = '#';
+                    lowestRowWithEmptyCell--;
                 }
-                if(box[i][j] == '#'){
-                    box[i][j] = '.';
-                    box[i][empty] = '#';
-                    --empty;
+                // Found an obstacle - reset `lowestRowWithEmptyCell` to the row directly above it
+                if (box[i][j] == '*') {
+                    // Place the obstacle in the correct position in the rotated grid
+                    result[j][m - i - 1] = '*';
+                    lowestRowWithEmptyCell = j - 1;
                 }
             }
         }
-        
-        for(int i = 0; i<r; ++i){
-            for(int j = c-1; j>=0; --j)
-                box2[j][r-i-1] = box[i][j];
-        }
-        
-        return box2;
+        return result;
     }
 }
