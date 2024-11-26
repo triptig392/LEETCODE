8dfe520d9412f1a8d450b3fed8cf702184c633c1
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-11-26 22:04:28 +0530

    Time: 6 ms (16.48%), Space: 46 MB (25%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,5 +1,10 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
+
+        if(n== 1 ){
+            return 0; 
+        }
+
         int[][] arr = new int[n][2];
 
         for(int i=0; i<n; i++){
@@ -11,17 +16,7 @@ class Solution {
         }
 
         Arrays.sort(arr, (a, b)-> a[0]-b[0]);
-
         
-
-        for(int i=0; i<n; i++){
-            System.out.println(arr[i][0] + " " + arr[i][1]);
-        }
-
-        if(n== 1 ){
-            return 0; 
-        }
-
         if( arr[n-1][0] != arr[n-2][0] ){
             return arr[n-1][1];
         }
