137aa3253b96ca2dda79d128e830faba9f82c6c9
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2024-12-01 00:47:26 +0530

    Time: 328 ms (8.12%), Space: 72.9 MB (13.79%) - LeetHub

---
+++ 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPossibleToRearrange(String s, String t, int k) {
+        List<String> l1 = new ArrayList<>();
+        List<String> l2 = new ArrayList<>();
+
+        int bp = s.length()/k;
+
+        for(int i=0; i<k; i++){
+            l1.add(s.substring(i*bp, (i+1)*bp));
+            l2.add(t.substring(i*bp, (i+1)*bp));
+        }
+
+        Collections.sort(l1);
+        Collections.sort(l2);
+
+        System.out.println(l1);
+         System.out.println(l2);
+
+        for(int i=0; i<l1.size(); i++){
+            if(!l1.get(i).equals(l2.get(i))){
+                return false;
+            }
+        }
+
+        
+        return true;
+    }
+}
