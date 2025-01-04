29af570a68bbab2894092e6174d7a03908d6e6df
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-01-04 22:11:02 +0530

    Time: 202 ms (52.09%), Space: 45.5 MB (47.91%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,14 +1,32 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-    int first[] = new int[26], last[] = new int[26], res = 0;
-    Arrays.fill(first, Integer.MAX_VALUE);
-    for (int i = 0; i < s.length(); ++i) {
-        first[s.charAt(i) - 'a'] = Math.min(first[s.charAt(i) - 'a'], i);
-        last[s.charAt(i) - 'a'] = i;
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (first[curr] == - 1) {
+                first[curr] = i;
+            }
+            
+            last[curr] = i;
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (first[i] == -1) {
+                continue;
+            }
+            
+            Set<Character> between = new HashSet();
+            for (int j = first[i] + 1; j < last[i]; j++) {
+                between.add(s.charAt(j));
+            }
+            
+            ans += between.size();
+        }
+        
+        return ans;
     }
-    for (int i = 0; i < 26; ++i)
-        if (first[i] < last[i])
-            res += s.substring(first[i] + 1, last[i]).chars().distinct().count();
-    return res;
-}
 }
