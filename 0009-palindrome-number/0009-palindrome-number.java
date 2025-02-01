dbb26ff822c753c9340f2075d0b73fc5ee2b982d
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-01 23:54:53 +0530

    Time: 4 ms (100%), Space: 44.4 MB (18.29%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int org = x;
+        int rev = 0;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        while(x != 0){
+            int rem = x%10;
+            rev = rev*10 + rem;
+            x = x/10;
+        }
+        
+        if(rev > max){
+            return false;
+        }
+        if(rev == org){
+            return true;
+        }
+        return false;
+    }
+}
