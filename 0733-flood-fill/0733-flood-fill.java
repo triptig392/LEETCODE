0656e47b616c2b7798e6c237235116ea134121a6
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-02 22:17:45 +0530

    Time: 0 ms (100%), Space: 45 MB (66.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,14 +4,16 @@ class Solution {
             return;
         }
 
-        if(sr >= 0 && sr <arr.length && sc >=0 && sc < arr[0].length && arr[sr][sc] == initial){
-            arr[sr][sc]= color;
-
-            dfs(arr, sr-1, sc, color, initial);
-            dfs(arr, sr, sc+1, color, initial);
-            dfs(arr, sr+1, sc, color, initial);
-            dfs(arr, sr, sc-1, color, initial);
+        if(sr < 0 || sr >= arr.length || sc < 0 || sc >= arr[0].length || arr[sr][sc] != initial){
+            return;
         }
+
+        arr[sr][sc]= color;
+
+        dfs(arr, sr-1, sc, color, initial);
+        dfs(arr, sr, sc+1, color, initial);
+        dfs(arr, sr+1, sc, color, initial);
+        dfs(arr, sr, sc-1, color, initial);
     }
 
     public int[][] floodFill(int[][] arr, int sr, int sc, int color) {
