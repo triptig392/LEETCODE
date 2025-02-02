5def690a2f8a684d3017442b0ec92fff21484c3d
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-02 22:05:41 +0530

    Time: 0 ms (100%), Space: 45 MB (66.47%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void dfs(int[][] arr, int sr, int sc, int color, int initial, int[] r, int[] c){
+
+        arr[sr][sc]= color;
+
+        for(int i=0; i<4; i++){
+            int row = sr+r[i];
+            int col = sc+c[i];
+            if(row >=0 && row < arr.length && col >=0 && col < arr[0].length ){
+                if(arr[row][col] == initial && arr[row][col] != color){
+                    dfs(arr, row, col, color, initial, r, c);
+                }
+            }
+        }
+    }
+
+    public int[][] floodFill(int[][] arr, int sr, int sc, int color) {
+        int initial = arr[sr][sc];
+        int[] r = {-1, 0, +1, 0};
+        int[] c = {0, +1, 0, -1};
+        dfs(arr, sr, sc, color, initial, r, c);
+        return arr;
+    }
+}
