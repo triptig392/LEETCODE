94cafb0569c3b0cea7ef8aa7eb34df37eac551af
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-02 22:22:06 +0530

    Time: 0 ms (100%), Space: 44.6 MB (98.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,26 @@
 class Solution {
-    public void dfs(int[][] arr, int sr, int sc, int color, int initial){
-        int n = arr.length;
-        int m = arr[0].length;
-        if(sr < 0 || sr >= n|| sc < 0 || sc >= m || arr[sr][sc] != initial){
-            return;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
         }
-
-        arr[sr][sc]= color;
-
-        dfs(arr, sr-1, sc, color, initial);
-        dfs(arr, sr, sc+1, color, initial);
-        dfs(arr, sr+1, sc, color, initial);
-        dfs(arr, sr, sc-1, color, initial);
+        return image;
     }
-
-    public int[][] floodFill(int[][] arr, int sr, int sc, int color) {
-        int initial = arr[sr][sc];
-        if(initial == color){
-            return arr;
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) {
+                dfs(image, r - 1, c, color, newColor);
+            }
+            if (c >= 1) {
+                dfs(image, r, c - 1, color, newColor);
+            }
+            if (r + 1 < image.length) {
+                dfs(image, r + 1, c, color, newColor);
+            }
+            if (c + 1 < image[0].length) {
+                dfs(image, r, c + 1, color, newColor);
+            }
         }
-        dfs(arr, sr, sc, color, initial);
-        return arr;
     }
 }
