0dc5da8fc2f9e716ebb1d242ae57d83d311164a0
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-05 15:27:45 +0530

    Time: 0 ms (100%), Space: 56.3 MB (59.11%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,29 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int si = 0;
-        int ei = arr.length-1;
-        int mid = 0;
+        int start = 0; 
+        int end = arr.length-1;
 
-        while(si<=ei){
-            if(ei-si < 2){
-                if(si != -1 && ei != -1 && arr[si]>arr[ei]){
-                    return si;
-                }else{
-                    return ei;
-                }
+        while (start <end ){
+            int mid = start + (end - start)/2;
+            if(arr[mid]>arr[mid +1]){
+            end = mid ;
             }
-
-            mid = (si+ei)/2;
-
-            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
-                return mid;
-            }else if(arr[mid] < arr[mid-1]){
-                ei = mid-1;
-            }else{
-                si=mid+1;
+            else{
+                start= mid +1;
             }
         }
-
-        return si;
+        return start ; //or you can return end both are pointing to the same element
     }
 }
