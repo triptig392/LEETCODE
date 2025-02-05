6f87d4e0a870e0994c4dcdb27b6a2c8419f9c6b6
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-05 14:59:30 +0530

    Time: 0 ms (100%), Space: 42 MB (9.84%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        int swap = 0;
+        int swapIdx = -1;
+        int swapIdx2 = -1;
+
+        for(int i=0; i<s1.length();i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                swap++;
+                if(swap == 1){
+                    swapIdx = i;
+                }
+                if(swap == 2){
+                    swapIdx2 = i;
+                }
+            }
+            if(swap > 2){
+                return false;
+            }
+        }
+
+        if(swapIdx == -1 && swapIdx2 == -1){
+            return true;
+        }
+
+        if(swapIdx2 == -1 && swapIdx != -1){
+            return false;
+        }
+
+        if(s1.charAt(swapIdx) != s2.charAt(swapIdx2) || 
+        s1.charAt(swapIdx2) != s2.charAt(swapIdx)){
+            return false;
+        }
+
+        return true;
+    }
+}
