e2af3b6d21e5878580235c1bea6ef0b033cf55dc
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-09 00:24:07 +0530

    Time: 1 ms (100%), Space: 42.7 MB (38.3%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -24,7 +24,6 @@ class Solution {
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
                 if(arr[i][j] == 1){
-                     System.out.println("y");
                     return -1;
                 }
                 ans = Math.max(arr[i][j], ans);
