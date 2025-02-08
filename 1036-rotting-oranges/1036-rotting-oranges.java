d2064bb09f109475644b2e9f28f611a3e6c6dca8
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-09 00:11:05 +0530

    Time: 2 ms (71.01%), Space: 42 MB (96.55%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,14 +1,16 @@
 class Solution {
     public void bfs(int[][] arr, int r, int c, int d){
-        if(r>= arr.length || c >=arr[0].length || r <0 || c <0 || arr[r][c] == 0 || 
-          (1<arr[r][c] && arr[r][c]<d)){
+        if(r>= arr.length || c >=arr[0].length || r <0 || c <0 || arr[r][c] == 0 ){
             return;
         }
+
+        if(arr[r][c] >= d || arr[r][c] == 1){
             arr[r][c] = d;
             bfs(arr, r+1, c, d+1);
             bfs(arr,r, c+1, d+1);
             bfs(arr,r-1, c, d+1);
             bfs(arr,r, c-1, d+1);
+        }
     }
     public int orangesRotting(int[][] arr) {
         for(int i=0; i<arr.length; i++){
@@ -22,6 +24,7 @@ class Solution {
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
                 if(arr[i][j] == 1){
+                     System.out.println("y");
                     return -1;
                 }
                 ans = Math.max(arr[i][j], ans);
