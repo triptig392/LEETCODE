0b2ad927286823046297dc3b2b3f09dc4df4de3a
TRIPTI GUPTA <120786520+triptig392@users.noreply.github.com>
2025-02-09 22:15:25 +0530

    Time: 83 ms (54.36%), Space: 98.1 MB (79.87%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,25 +1,41 @@
 class NumberContainers {
-    Map<Integer,Integer> m;
-    Map<Integer,PriorityQueue<Integer>> d;
+
+    // Maps from number to set of indices and from index to number
+    private Map<Integer, TreeSet<Integer>> numberToIndices;
+    private Map<Integer, Integer> indexToNumbers;
+
+    // Constructor
     public NumberContainers() {
-        m = new HashMap<>();
-        d = new HashMap<>();
+        // Initialize the data structures
+        numberToIndices = new HashMap<>();
+        indexToNumbers = new HashMap<>();
     }
-    public void change(int i, int n) {
-        if(m.containsKey(i) && m.get(i)==n)return;
-        m.put(i,n);
-        d.computeIfAbsent(n,k->new PriorityQueue<>()).offer(i);
+
+    public void change(int index, int number) {
+        // If index already has a number, remove it from the old number's index set
+        if (indexToNumbers.containsKey(index)) {
+            int previousNumber = indexToNumbers.get(index);
+            numberToIndices.get(previousNumber).remove(index);
+            if (numberToIndices.get(previousNumber).isEmpty()) {
+                numberToIndices.remove(previousNumber);
+            }
+        }
+        // Update the number and add the index to the new number's set
+        indexToNumbers.put(index, number);
+        numberToIndices.computeIfAbsent(number,k -> new TreeSet<>()).add(index);
     }
-    public int find(int n) {
-        if(!d.containsKey(n)) return -1;
-        PriorityQueue<Integer> pq = d.get(n);
-        while(!pq.isEmpty() && m.get(pq.peek())!=n) pq.poll();
-        return pq.isEmpty()? -1 : pq.peek();
+
+    public int find(int number) {
+        // Return the smallest index for the given number, or -1 if not found
+        if (numberToIndices.containsKey(number)) {
+            return numberToIndices.get(number).first(); // Get the smallest index
+        }
+        return -1;
     }
 }
 /**
  * Your NumberContainers object will be instantiated and called as such:
  * NumberContainers obj = new NumberContainers();
- * obj.change(index,number);
+ * obj.change(index, number);
  * int param_2 = obj.find(number);
  */
